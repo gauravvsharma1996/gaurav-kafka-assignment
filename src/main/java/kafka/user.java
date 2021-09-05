457e37f82753f1a47c1ed7130b78e0d00ba1b56d
gauravvsharma1996 <gaurav.sharma@knoldus.com>
2021-09-05 22:12:46 +0530

    initial commit

---
+++ src/main/java/kafka/user.java
@@ -0,0 +1,23 @@
+package kafka;
+
+import org.apache.kafka.common.protocol.types.Field;
+
+public class user {
+
+    int id, age;
+    String name, course;
+    public user(int id, String name, int age, String course) {
+
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.course = course;
+
+    }
+
+    public String toString() {
+
+        return ("\"Id\":"+"\""+Integer.toString(id)+"\","+"\"Name\":"+"\""+name+"\","+"\"Age\":"+"\""+Integer.toString(age)+"\","+"\"Course\":"+"\""+course+"\"");
+    }
+
+}
